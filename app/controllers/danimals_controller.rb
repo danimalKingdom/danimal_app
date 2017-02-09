40be64db3c9b7b7896d47a993bef1a1a3c442935
dani <dani.zaghian@gmail.com>
2017-02-08 20:17:45 -0800

    added new,create,show to danimals

--- app/controllers/danimals_controller.rb
+++ app/controllers/danimals_controller.rb
@@ -2,4 +2,22 @@ class DanimalsController < ApplicationController
   def index
     @danimals = Danimal.all
   end
+
+  def new
+  end
+
+  def create
+    danimal = Danimal.create(
+      species: params[:species],
+      leg_count: params[:leg_count]
+      )
+
+    redirect_to danimal
+    #redirect_to danimal_path(danimal)
+    #redirect_to '/danimals/#{danimal.id}'
+  end
+
+  def show
+    @danimal = Danimal.find_by(id: params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   #request_type "/path" => "controller#method", as: "prefix"
   root to: 'danimals#index'
-  #get '/' => 'danimals#index', as: 'danimals_path'
+  #get '/' => 'danimals#index', as: 'root'
 
-  resources :danimals, only: [:index]
-  #get '/danimals' => 'danimals#index'
+  resources :danimals, only: [:index, :new, :create, :show]
+  #get '/danimals' => 'danimals#index', as: 'danimals'
+  #get '/danimals/new' => 'danimals#new', as: 'new_danimal'
+  #post '/danimals/create' => 'danimals#create'
+  #get '/danimals/:id' => 'danimals#show', as: 'danimal'
+
 end
